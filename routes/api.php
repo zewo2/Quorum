9122cd866adf82a0bcb7e47d2e8ecb84d8876365
zewo2 <191111034+zewo2@users.noreply.github.com>
2026-02-19 02:09:35 +0000

    public api fixes

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,14 @@ use App\Http\Controllers\Api\SubjectApiController;
 use App\Http\Controllers\Api\DepartmentApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('quorum/api')->group(function () {
-    // Courses API
-    Route::get('/courses', [CourseApiController::class, 'index']);
-    Route::get('/courses/{id}', [CourseApiController::class, 'show']);
+// Courses API
+Route::get('/courses', [CourseApiController::class, 'index']);
+Route::get('/courses/{id}', [CourseApiController::class, 'show']);
 
-    // Subjects API
-    Route::get('/subjects', [SubjectApiController::class, 'index']);
-    Route::get('/subjects/{id}', [SubjectApiController::class, 'show']);
+// Subjects API
+Route::get('/subjects', [SubjectApiController::class, 'index']);
+Route::get('/subjects/{id}', [SubjectApiController::class, 'show']);
 
-    // Departments API
-    Route::get('/departments', [DepartmentApiController::class, 'index']);
-    Route::get('/departments/{id}', [DepartmentApiController::class, 'show']);
-});
+// Departments API
+Route::get('/departments', [DepartmentApiController::class, 'index']);
+Route::get('/departments/{id}', [DepartmentApiController::class, 'show']);
