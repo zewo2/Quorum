bbb7a031dbfbfdb35975ae67d8e8410d7c21131d
zewo2 <191111034+zewo2@users.noreply.github.com>
2026-02-23 15:15:56 +0000

    added tab icon

--- resources/views/layouts/fe_master.blade.php
+++ resources/views/layouts/fe_master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Quorum - Campus Management')</title>
+    <link rel="icon" type="image/png" href="{{ asset('Quorum_logo - no background.png') }}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
